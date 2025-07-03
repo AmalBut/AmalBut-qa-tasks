b70282e7c30b699a8ce2a375ca59280aae045d24
Amal Butmah <amalbutmah376@gmail.com>
2025-07-03 19:43:58 +0300

    Test case to submit review for a product

---
+++ cypress/e2e/CypressTask2Solution/submitReview.cy.js
@@ -0,0 +1,20 @@
+/// <reference types = "cypress" />
+
+describe('Submit a review for a product', () => {
+    before(() => {
+        cy.visit("/")
+        cy.get("#search").type("shirt {enter}")  
+        cy.get(".product-item-photo").first().click()
+    })
+
+    it('Validate that the user can submit a review on the product page', () => {
+        cy.get(".reviews-actions .add").click()
+        cy.get("#Rating_4.radio").check({force: true})
+        cy.get("#nickname_field").type("Amal")
+        cy.get("#summary_field").type("This is a Summary")
+        cy.get("#review_field").type("This is a Review")
+        cy.get(".review-form-actions .submit").click()
+        cy.get('[role="alert"]').should("be.visible").and("contain","You submitted your review for moderation.")
+    });
+    
+});
