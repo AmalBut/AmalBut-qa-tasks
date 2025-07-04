fd2e36420cccd0debe83f0c710693c8d0a95433f
Amal Butmah <amalbutmah376@gmail.com>
2025-07-04 10:25:11 +0300

    modify add to wishlist test case structure

--- cypress/e2e/CypressTask2Solution/addProductToWishlist.cy.js
+++ cypress/e2e/CypressTask2Solution/addProductToWishlist.cy.js
@@ -5,17 +5,20 @@ describe('', () => {
         const email = "CypressUser@gmail.com"
         const password = "test@123"
         cy.loginToMagento(email,password)  //Custom command
-        cy.get("#search").type("shirt {enter}")
-        cy.get(".product-item-info").first().click()
     })
     it('Validate that the user can add the product to the wishlist', () => {
         //Actions
+        cy.get("#search").type("shirt {enter}")
+        cy.get(".product-item-info").first().click()
         cy.get(".towishlist").click()
         cy.screenshot({capture:"fullPage"})
 
         //Assertions
-        cy.get('[role="alert"]').should("be.visible").and("contain","has been added to your Wish List. Click here to continue shopping.")
-        cy.url().should("eq","https://magento.softwaretestingboard.com/wishlist/index/index/wishlist_id/1111/")
+        cy.get('[role="alert"]')
+          .should("be.visible")
+          .and("contain","has been added to your Wish List. Click here to continue shopping.")
+          
+        cy.url().should("include", "/wishlist/index/index")
         cy.get(".page-title").should("contain","My Wish List")
     });
 });
