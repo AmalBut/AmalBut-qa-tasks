eda45de7196d9bc7d87432da470252217496c78a
Amal Butmah <amalbutmah376@gmail.com>
2025-07-04 10:10:04 +0300

    Clarify the actions and assertions

--- cypress/e2e/CypressTask2Solution/addProductToWishlist.cy.js
+++ cypress/e2e/CypressTask2Solution/addProductToWishlist.cy.js
@@ -6,14 +6,16 @@ describe('', () => {
         const password = "test@123"
         cy.loginToMagento(email,password)  //Custom command
         cy.get("#search").type("shirt {enter}")
-        cy.contains(".product-item-info","Radiant Tee").click()
+        cy.get(".product-item-info").first().click()
     })
     it('Validate that the user can add the product to the wishlist', () => {
+        //Actions
         cy.get(".towishlist").click()
         cy.screenshot({capture:"fullPage"})
-        cy.get('[role="alert"]').should("be.visible").and("contain","Radiant Tee has been added to your Wish List. Click here to continue shopping.")
+
+        //Assertions
+        cy.get('[role="alert"]').should("be.visible").and("contain","has been added to your Wish List. Click here to continue shopping.")
         cy.url().should("eq","https://magento.softwaretestingboard.com/wishlist/index/index/wishlist_id/1111/")
-        cy.contains(".product-item-info","Radiant Tee").should("be.visible")
         cy.get(".page-title").should("contain","My Wish List")
     });
 });
